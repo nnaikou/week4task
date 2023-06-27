7a96a1fbd0db8a061d52da8db05017c676f7f89a
NikoS <122801074+nnaikou@users.noreply.github.com>
2023-06-27 18:30:46 +0000

    Update 6 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,58 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+
+
+let inputText = document.getElementById("input-show")
+let button = document.getElementById("submit-data")
+
+button.addEventListener("click", () => {
+  getdata()
+})
+
+
+
+
+async function getdata() {
+
+  let url = "https://api.tvmaze.com/search/shows?q="+ inputText.value
+  let dataPromise = await fetch(url)
+  let dataJSON = await dataPromise.json()
+
+  const data0 = Object.values(dataJSON)
+  let showAllShows = document.getElementById("all-shows-data")
+
+  data0.forEach((x) => {
+
+    let showData = document.createElement("div")
+    showData.setAttribute("class", "show-data")
+
+    let showInfo = document.createElement("div")
+    showInfo.setAttribute("class", "show-info")
+
+    let showTitle = document.createElement("h1")
+    showTitle.innerText = x.show.name
+
+    let showSummary = document.createElement("p")
+    showSummary.innerHTML = x.show.summary
+
+    let pic = document.createElement("img")
+    try {
+      pic.setAttribute("src", x.show.image.medium)
+    } catch {
+      pic.setAttribute("src", "null")
+    }
+
+  
+    showInfo.appendChild(showTitle)
+    showInfo.appendChild(showSummary)
+    showData.appendChild(pic)
+    showData.appendChild(showInfo)
+
+    showAllShows.appendChild(showData)
+
+
+  
+  })
+
+
+}
